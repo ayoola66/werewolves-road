fbdccf4b03e4c001c4363ca26ce6ac73652ecd95
Ayo Ogunrekun <ayoola66@gmail.com>
2025-07-28 22:16:44 +0100

    Update environment configuration

--- server/env.ts
+++ server/env.ts
@@ -1,1 +1,11 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  DATABASE_URL: process.env.DATABASE_URL || "",
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || "8080",
+  HOST: process.env.HOST || "0.0.0.0",
+  SENTRY_DSN: process.env.SENTRY_DSN || "",
+};
